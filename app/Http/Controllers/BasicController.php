b6ad3e72886efe38e8c894bf51955eb7238f4497
Saheed <saolabram@gmail.com>
2024-07-11 16:14:35 +0100

    input ip

--- app/Http/Controllers/BasicController.php
+++ app/Http/Controllers/BasicController.php
@@ -12,8 +12,9 @@ class BasicController extends Controller
         // Get visitor name from query parameter
         $visitorName = $request->input('visitor_name', 'Guest');
 
+        $clientIp = $request->ip();
         // Fetch location based on IP address (using a service like ip-api.com)
-        $location = $this->getLocationByIp();
+        $location = $this->getLocationByIp($clientIp);
         $temperature = $this->getTemperature($location);
 
         // Prepare response data
@@ -26,13 +27,17 @@ class BasicController extends Controller
         return response()->json($response);
     }
 
-    private function getLocationByIp()
+    public function getLocationByIp($ip)
     {
         $client = new Client();
-        $response = $client->get("http://ip-api.com/json");
+
+        $response = $client->get("http://ip-api.com/json/{$ip}");
         $data = json_decode($response->getBody(), true);
 
+        // dd($response);
+
         return $data;
+
     }
     
     private function getTemperature($getLoc)

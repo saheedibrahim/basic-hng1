47e6c8010e8c0deb43032889d6a56eba79e5c11a
Saheed <saolabram@gmail.com>
2024-07-13 12:37:19 +0100

    update ip collection

--- app/Http/Controllers/BasicController.php
+++ app/Http/Controllers/BasicController.php
@@ -26,19 +26,39 @@ class BasicController extends Controller
 
         return response()->json($response);
     }
-
+    
     private function getLocationByIp($ip)
     {
+        // Validate if $ip is a client IP or server IP
+        if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+            // It's likely a client IP, use it directly
+            $clientIp = $ip;
+        } else {
+            // Use $_SERVER['REMOTE_ADDR'] as a fallback
+            $clientIp = $_SERVER['REMOTE_ADDR'];
+        }
+
         $client = new Client();
 
-        $response = $client->get("http://ip-api.com/json/{$ip}");
+        $response = $client->get("http://ip-api.com/json/{$clientIp}");
         $data = json_decode($response->getBody(), true);
 
-        // dd($response);
-
         return $data;
-
     }
+
+
+    // private function getLocationByIp($ip)
+    // {
+    //     $client = new Client();
+
+    //     $response = $client->get("http://ip-api.com/json/{$ip}");
+    //     $data = json_decode($response->getBody(), true);
+
+    //     // dd($response);
+
+    //     return $data;
+
+    // }
     
     private function getTemperature($getLoc)
     {
